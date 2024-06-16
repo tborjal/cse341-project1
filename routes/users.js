2d7f2468609aabe8cdeece5c3d93e11ee9f72a7a
tborjal <bor22014@byui.edu>
2024-06-16 10:04:47 +0800

    added rest file

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const usersController = require('../controllers/users');
 
-router. get('/', usersController.getAll);
+router.get('/', usersController.getAll);
 router.get('/:id', usersController.getSingle);
 
 module.exports = router;
