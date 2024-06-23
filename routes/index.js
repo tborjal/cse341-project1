f25eb919d302365524227e22e05b1cde7f487f81
tborjal <bor22014@byui.edu>
2024-06-23 11:58:03 +0800

    installed swagger and configured routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const router = require ('express').Router();
 
 // router.get('/',(req, res) => {res.send('Hello world')})
 
+router.use('/', require('./swagger'));
+
 router.use('/users', require('./users'));
 
 module.exports = router;
