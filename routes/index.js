09109ecb7103b52714ec2fca11f82440f9cc2fa7
tborjal <bor22014@byui.edu>
2024-06-23 12:20:36 +0800

    fixed issues

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const router = require ('express').Router();
 
-// router.get('/',(req, res) => {res.send('Hello world')})
-
 router.use('/', require('./swagger'));
+
+router.get('/',(req, res) => {
+//#swagger.tags=['Hello-World]
+    res.send('Hello world');
+});
 
 router.use('/users', require('./users'));
 
